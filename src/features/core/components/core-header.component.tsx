097013b9e52e6da4df577efaf6a5642480562155
Erwin Errol Calanuga <hortsatta@gmail.com>
2024-04-01 18:39:48 +0800

    Add env variables

--- src/features/core/components/core-header.component.tsx
+++ src/features/core/components/core-header.component.tsx
@@ -2,6 +2,12 @@ import { memo } from 'react';
 
 import type { ComponentProps } from 'react';
 
+const APP_TITLE = import.meta.env.VITE_APP_TITLE || 'Typen';
+
 export const CoreHeader = memo(function (props: ComponentProps<'header'>) {
-  return <header {...props}>Typen | Keyboard Typing Test</header>;
+  return (
+    <header {...props}>
+      <h1>{APP_TITLE}</h1>
+    </header>
+  );
 });
