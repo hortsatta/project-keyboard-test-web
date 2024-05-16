3546ba10f439985a56518f43b6377592a876a9ea
Erwin Errol Calanuga <hortsatta@gmail.com>
2024-05-16 20:50:27 +0800

    Add seo

--- src/features/app.tsx
+++ src/features/app.tsx
@@ -1,7 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import { router } from './routes';
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
---
+++ src/features/base/components/base-seo.component.tsx
@@ -0,0 +1,42 @@
+import { Helmet } from 'react-helmet-async';
+
+import { useMemo, type ComponentProps } from 'react';
+
+type Props = ComponentProps<typeof Helmet> & {
+  title?: string;
+  description?: string;
+};
+
+const APP_URL = import.meta.env.VITE_APP_URL;
+const APP_TITLE = import.meta.env.VITE_APP_TITLE || 'Typen';
+const APP_DESCRIPTION = import.meta.env.VITE_APP_DESCRIPTION;
+
+export function BaseSEO({ title, description, ...moreProps }: Props) {
+  const currentTitle = useMemo(() => {
+    if (title?.trim()) {
+      return `${title} / ${APP_TITLE}`;
+    }
+
+    return `${APP_TITLE} / ${APP_DESCRIPTION}`;
+  }, [title]);
+
+  return (
+    <Helmet {...moreProps}>
+      <title>{currentTitle}</title>
+      <link rel='canonical' href={APP_URL} />
+      {/* OpenGraph Tags */}
+      <meta name='description' content={description || APP_DESCRIPTION} />
+      <meta property='og:title' content={title || APP_TITLE} />
+      <meta
+        property='og:description'
+        content={description || APP_DESCRIPTION}
+      />
+      {/* Twitter Card Tags */}
+      <meta name='twitter:title' content={title || APP_TITLE} />
+      <meta
+        name='twitter:description'
+        content={description || APP_DESCRIPTION}
+      />
+    </Helmet>
+  );
+}
