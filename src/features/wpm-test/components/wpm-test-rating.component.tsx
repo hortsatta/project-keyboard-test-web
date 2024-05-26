6c831f637545108b7b43435538892e4c1d8235a1
Erwin Errol Calanuga <hortsatta@gmail.com>
2024-05-27 02:14:13 +0800

    Fix rating image not showing

--- src/features/wpm-test/components/wpm-test-rating.component.tsx
+++ src/features/wpm-test/components/wpm-test-rating.component.tsx
@@ -3,20 +3,6 @@ import cx from 'classix';
 
 import type { ComponentProps } from 'react';
 
-import ratingSSSSvg from '#/assets/images/rating-sss.svg';
-import ratingSSvg from '#/assets/images/rating-s.svg';
-import ratingASvg from '#/assets/images/rating-a.svg';
-import ratingBSvg from '#/assets/images/rating-b.svg';
-import ratingCSvg from '#/assets/images/rating-c.svg';
-import ratingDSvg from '#/assets/images/rating-d.svg';
-import ratingESvg from '#/assets/images/rating-e.svg';
-import ratingSSSStrokeSvg from '#/assets/images/rating-sss-stroke.svg';
-import ratingSStrokeSvg from '#/assets/images/rating-s-stroke.svg';
-import ratingAStrokeSvg from '#/assets/images/rating-a-stroke.svg';
-import ratingBStrokeSvg from '#/assets/images/rating-b-stroke.svg';
-import ratingCStrokeSvg from '#/assets/images/rating-c-stroke.svg';
-import ratingDStrokeSvg from '#/assets/images/rating-d-stroke.svg';
-import ratingEStrokeSvg from '#/assets/images/rating-e-stroke.svg';
 import ratingBgPng from '#/assets/images/rating-bg.png';
 
 type Props = ComponentProps<'div'> & {
@@ -29,51 +15,36 @@ const ratingBgStyle = {
 
 const ratings = {
   e: {
-    img: ratingESvg,
-    strokeImg: ratingEStrokeSvg,
     color: 'green',
     gradient: '#45d94a, #45d94a, #2b8b2e, #45d94a',
     strokeGradient: '#51ff57, #37b13b, #37b13b, #51ff57',
   },
   d: {
-    img: ratingDSvg,
-    strokeImg: ratingDStrokeSvg,
     color: 'orange',
     gradient: '#ff841f, #ff841f, #b65200, #ff841f',
     strokeGradient: '#ff9f51, #e86900, #e86900, #ff9f51',
   },
   c: {
-    img: ratingCSvg,
-    strokeImg: ratingCStrokeSvg,
-
     color: 'red',
     gradient: '#f42b2b, #f42b2b, #ad0909, #f42b2b',
     strokeGradient: '#f65a5a, #dd0c0c, #dd0c0c, #f65a5a',
   },
   b: {
-    img: ratingBSvg,
-    strokeImg: ratingBStrokeSvg,
     color: 'yellow',
     gradient: '#ffc91f, #ffc91f, #b68a00, #ffc91f',
     strokeGradient: '#ffd551, #e8b000, #e8b000, #ffd551',
   },
   a: {
-    img: ratingASvg,
-    strokeImg: ratingAStrokeSvg,
     color: 'violet',
     gradient: '#7d2bf4, #7d2bf4, #4c09ad, #7d2bf4',
     strokeGradient: '#9a5af6, #610cdd, #610cdd, #9a5af6',
   },
   s: {
-    img: ratingSSvg,
-    strokeImg: ratingSStrokeSvg,
     color: 'white',
     gradient: '#dfdcdc, #dfdcdc, #8a8a8a, #dfdcdc',
     strokeGradient: '#ffffff, #b2b2b2, #b2b2b2, #ffffff',
   },
   sss: {
-    img: ratingSSSSvg,
-    strokeImg: ratingSSSStrokeSvg,
     color: 'green',
     gradient: '#45d94a, #45d94a, #2b8b2e, #45d94a',
     strokeGradient: '#51ff57, #37b13b, #37b13b, #51ff57',
@@ -85,22 +56,6 @@ export const WPMTestRating = memo(function ({
   rating,
   ...moreProps
 }: Props) {
-  const ratingStyle = useMemo(
-    () => [
-      {
-        maskImage: `url(${(ratings as any)[rating].strokeImg})`,
-        maskRepeat: 'no-repeat',
-        maskPosition: '50%',
-      },
-      {
-        maskImage: `url(${(ratings as any)[rating].img})`,
-        maskRepeat: 'no-repeat',
-        maskPosition: '50%',
-      },
-    ],
-    [rating],
-  );
-
   const ratingGradientStyle = useMemo(
     () => [
       {
@@ -118,7 +73,12 @@ export const WPMTestRating = memo(function ({
   return (
     <div className={cx('relative', className)} {...moreProps}>
       <div className='relative z-10 animate-rating-reveal'>
-        <div style={ratingStyle[0]} className='absolute left-0 top-0 z-10'>
+        <div
+          className={cx(
+            'rating-mask absolute left-0 top-0 z-10',
+            `rating-${rating}-stroke`,
+          )}
+        >
           <div
             style={ratingGradientStyle[0]}
             className={cx(
@@ -129,7 +89,7 @@ export const WPMTestRating = memo(function ({
             )}
           />
         </div>
-        <div style={ratingStyle[1]}>
+        <div className={cx('rating-mask', `rating-${rating}`)}>
           <div
             style={ratingGradientStyle[1]}
             className={cx(
